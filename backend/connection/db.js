6621b4fac733e660563fc324cc2df3c9218bd084
ChavadaHarsh <harshchavada@techmatrix.io>
2025-10-28 10:23:10 +0530

    connection update.

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Task", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://chavadaharsh73_db_user:fcZoduVaaiz4pkZU@cluster0.wl822dd.mongodb.net/Task",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
